6d69dd27671c88de22129e12ae44503bf39d247b
ex.marin <ex.marinm@gmail.com>
2018-04-25 20:05:02 -0300

    Ejercicio 2 resuelto

--- 2 ciclos/ejercicio2.rb
+++ 2 ciclos/ejercicio2.rb
@@ -1,7 +1,10 @@
 # En el siguiente código reemplaza la instrucción 'while' por 'times'.
 
 i = 0
-while i < 10
-  puts "Iteración #{i}"
-  i = i + 1
+
+10.times do |i|
+  if i <= 10
+    puts "Iteración #{i}"
+    i = i + 1
+  end
 end
