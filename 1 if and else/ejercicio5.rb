634835136b75baabe05955545be0b6c4a24f1d14
ex.marin <ex.marinm@gmail.com>
2018-04-25 20:45:30 -0300

    Ejercicio 5 resuelto

--- 1 if and else/ejercicio5.rb
+++ 1 if and else/ejercicio5.rb
@@ -1,9 +1,8 @@
 # Utiliza álgebra booleana para hacer un refactoring de este código
 # Para verificar la respuestas, puedes variar los valores de a y b.
 
-a = false
-b = true
-
+a = true
+b = false
 
 puts 'Lograste A y B!' if a && b
 
