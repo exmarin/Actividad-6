21145ff2cb5b8e8816ef484136692ff236c6fd8c
ex.marin <ex.marinm@gmail.com>
2018-04-25 21:08:16 -0300

    Ejercicio 8 resuelto

--- 2 ciclos/ejercicio10.rb
+++ 2 ciclos/ejercicio10.rb
@@ -2,9 +2,9 @@
 # e imprimirlo reiteradamente hasta que el usuario ingrese la opción número 4.
 opcion = 0
 while opcion != 4
-  puts "Opción 1: blah"
-  puts "Opción 2: blah"
-  puts "Opción 3: blah"
-  puts "Opción 4: Salir"
+puts "Opción 1: blah"
+puts "Opción 2: blah"
+puts "Opción 3: blah"
+puts "Opción 4: Salir"
 opcion = gets.chomp.to_i
 end
